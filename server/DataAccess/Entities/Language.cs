3bb79dbf0db1d3bc37ae153fcd695a9401dca06e
Frank Phan <frank.phan@helius-software.com>
2023-03-07 22:33:13 +0700

    Data schema using mysql

---
+++ server/DataAccess/Entities/Language.cs
@@ -0,0 +1,12 @@
+﻿namespace server.DataAccess.Entities
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDefault { get; set; }
+
+    }
+}
