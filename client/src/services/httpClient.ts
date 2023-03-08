ee8cb0f04c38be22f0c703b2821a16bebe8e79ea
Leo <luatvqph12182@fpt.edu.vn>
2023-03-08 17:25:01 +0700

    Add: module @fontsource/roboto

--- client/src/services/httpClient.ts
+++ client/src/services/httpClient.ts
@@ -4,13 +4,27 @@ const httpClient = axios.create({
     baseURL: `${import.meta.env.VITE_SERVICE}/api`,
 });
 
-httpClient.interceptors.response.use((response) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            return resolve(response);
-        }, 1000);
-    });
-});
+httpClient.interceptors.response.use(
+    (response) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                return resolve(response);
+            }, 1000);
+        });
+    },
+    (error) => {
+        console.log("Error::", error);
+        if (
+            error.response.status === 401 &&
+            error.response.data === "jwt expired"
+        ) {
+            window.alert("Login session has expired!");
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("user");
+            window.location.href = "/sign-in";
+        }
+    }
+);
 
 const setAuthorizationHeader = (token: string) => {
     httpClient.defaults.headers.common["Authorization"] = "Bearer " + token;
