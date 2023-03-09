e08a6b0411882cb02bfc7307c3c4d0dfbfe14e60
Frank Phan <frank.phan@helius-software.com>
2023-03-09 13:37:18 +0700

    add new api

---
+++ server/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok("ngon chim");
+        }
+    }
+}
