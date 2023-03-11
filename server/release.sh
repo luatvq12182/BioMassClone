c57256c90311d95313afca55b364cf6d837d4b44
Frank Phan <frank.phan@helius-software.com>
2023-03-12 03:20:22 +0700

    release.sh

--- server/release.sh
+++ server/release.sh
@@ -1,2 +1,4 @@
+sudo docker stop back-end-api
+sudo docker rm back-end-api
 sudo docker build -t green-way-api .
-sudo docker run -dp 80:80 green-way-api
+sudo docker run --name back-end-api -dp 80:80 green-way-api
