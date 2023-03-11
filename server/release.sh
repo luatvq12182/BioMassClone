af297b07ba0101c59e5ef664098746abd12297ce
Frank Phan <frank.phan@helius-software.com>
2023-03-12 03:22:59 +0700

    rm existed image

--- server/release.sh
+++ server/release.sh
@@ -1,4 +1,5 @@
 sudo docker stop back-end-api
 sudo docker rm back-end-api
+sudo docker image rm green-way-api
 sudo docker build -t green-way-api .
 sudo docker run --name back-end-api -dp 80:80 green-way-api
