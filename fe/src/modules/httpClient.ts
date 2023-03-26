e0a8c704b4dd80eb80a35800b0edef1f1cfa3eb3
Eren Yeager <eren_yeager@Erens-MacBook-Pro.local>
2023-03-26 14:31:31 +0700

    Fix error login

--- fe/src/modules/httpClient.ts
+++ fe/src/modules/httpClient.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const accessToken = JSON.parse(localStorage.getItem("accessToken") || "");
+const accessToken = localStorage.getItem("accessToken");
 
 const httpClient = axios.create({
     baseURL: `${import.meta.env.VITE_SERVICE}/api`,
     headers: {
-        Authorization: accessToken ? `Bearer ${accessToken}` : "Unset",
+        Authorization: accessToken ? `Bearer ${JSON.parse(accessToken)}` : "Unset",
     },
 });
 
