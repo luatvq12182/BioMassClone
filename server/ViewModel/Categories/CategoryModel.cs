570260a0a92292a52e65d284abb100f8725188fe
Frank Phan <frank.phan@helius-software.com>
2023-03-26 13:14:39 +0700

    remove catId

--- server/ViewModel/Categories/CategoryModel.cs
+++ server/ViewModel/Categories/CategoryModel.cs
@@ -3,7 +3,6 @@
     public class CategoryModel
     {
         public int Id { get; set; }
-        public int? CatLangId {get;set;}
         public int? LanguageId { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
