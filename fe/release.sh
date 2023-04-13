c6f09b9462745b608afc4a371ada312c8e5d920f
Eren Yeager <eren_yeager@Erens-MacBook-Pro.local>
2023-04-13 22:59:28 +0700

    fix port

--- fe/release.sh
+++ fe/release.sh
@@ -2,4 +2,4 @@ sudo docker stop green-way-cms-ins
 sudo docker rm green-way-cms-ins
 sudo docker image rm green-way-cms
 sudo docker build -t green-way-cms .
-sudo docker run --name green-way-cms-ins -dp 5173:5173 green-way-cms
+sudo docker run --name green-way-cms-ins -dp 3000:3000 green-way-cms
