af6f44acb56fbff0158527d25d08f22d5aef5efb
Eren Yeager <eren_yeager@Erens-MacBook-Pro.local>
2023-04-14 11:47:04 +0700

    change shell deploy

--- server/release.sh
+++ server/release.sh
@@ -1,5 +1,5 @@
-sudo docker stop back-end-api
-sudo docker rm back-end-api
+sudo docker stop green-way-api-ins
+sudo docker rm green-way-api-ins
 sudo docker image rm green-way-api
 sudo docker build -t green-way-api .
-sudo docker run --name back-end-api -dp 5240:80 green-way-api
+sudo docker run --name green-way-api-ins -dp 7000:80 green-way-api
